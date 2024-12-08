d826a91c7393e324d7b2dcc0a6632573ad9886c2
IsuruSoysa-ISG <isurumsoysa@gmail.com>
2024-12-08 14:05:47 +0530

    added sample calculator for api testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import SampleAddition from "./components/SampleAddition";
 
 function App() {
   return (
     <>
       <h1>Sustainability Web App</h1>
       <h4>Coming Soon...</h4>
+
+      <SampleAddition />
     </>
   );
 }
