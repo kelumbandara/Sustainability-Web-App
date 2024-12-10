7e18aac75e5c1d277ada13b0525137d9d18fb64b
IsuruSoysa-ISG <isurumsoysa@gmail.com>
2024-12-10 21:52:09 +0530

    remove unused 'react' in Image Carousel

--- client/src/components/ImageCarousel.tsx
+++ client/src/components/ImageCarousel.tsx
@@ -1,5 +1,5 @@
 import { useMediaQuery, useTheme } from "@mui/material";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const ImageCarousel = ({
   images,
