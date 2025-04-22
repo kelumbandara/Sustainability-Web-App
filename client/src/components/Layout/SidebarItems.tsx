696a70c60ec64e84e9afcac26cfb2f67342d70e9
IsuruSoysa-ISG <isurumsoysa@gmail.com>
2025-04-22 11:40:32 +0530

    hide unauthorized side nav items

--- client/src/components/Layout/SidebarItems.tsx
+++ client/src/components/Layout/SidebarItems.tsx
@@ -56,7 +56,6 @@ export const sidebarItems: Array<SidebarItem> = [
     href: "/home",
     icon: <HomeIcon fontSize="small" />,
     accessKey: PermissionKeys.INSIGHT_VIEW,
-    // Add the data-cy attribute here
   },
   {
     headline: "Administration",
@@ -317,46 +316,46 @@ export const sidebarItems: Array<SidebarItem> = [
     icon: <FolderIcon fontSize="small" />,
     accessKey: PermissionKeys.DOCUMENT_REGISTER_VIEW,
   },
-  {
-    title: "Equipment MNG",
-    href: "/equipment-mng",
-    icon: <ConstructionIcon fontSize="small" />,
-    disabled: true,
-    nestedItems: [
-      {
-        title: "Equipment",
-        href: "/equipment-mng/equipment",
-        icon: <SubdirectoryArrowRightIcon fontSize="small" />,
-        nestedItems: [
-          {
-            title: "Register",
-            href: "/equipment-mng/equipment/register",
-            icon: <SubdirectoryArrowRightIcon fontSize="small" />,
-          },
-          {
-            title: "Create",
-            href: "/equipment-mng/equipment/create",
-            icon: <SubdirectoryArrowRightIcon fontSize="small" />,
-          },
-        ],
-      },
-      {
-        title: "Client",
-        href: "/equipment-mng/client",
-        icon: <SubdirectoryArrowRightIcon fontSize="small" />,
-      },
-      {
-        title: "GEO Tag",
-        href: "/equipment-mng/geo-tag",
-        icon: <SubdirectoryArrowRightIcon fontSize="small" />,
-      },
-      {
-        title: "Inspection Template",
-        href: "/equipment-mng/inspection-template",
-        icon: <SubdirectoryArrowRightIcon fontSize="small" />,
-      },
-    ],
-  },
+  // {
+  //   title: "Equipment MNG",
+  //   href: "/equipment-mng",
+  //   icon: <ConstructionIcon fontSize="small" />,
+  //   disabled: true,
+  //   nestedItems: [
+  //     {
+  //       title: "Equipment",
+  //       href: "/equipment-mng/equipment",
+  //       icon: <SubdirectoryArrowRightIcon fontSize="small" />,
+  //       nestedItems: [
+  //         {
+  //           title: "Register",
+  //           href: "/equipment-mng/equipment/register",
+  //           icon: <SubdirectoryArrowRightIcon fontSize="small" />,
+  //         },
+  //         {
+  //           title: "Create",
+  //           href: "/equipment-mng/equipment/create",
+  //           icon: <SubdirectoryArrowRightIcon fontSize="small" />,
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       title: "Client",
+  //       href: "/equipment-mng/client",
+  //       icon: <SubdirectoryArrowRightIcon fontSize="small" />,
+  //     },
+  //     {
+  //       title: "GEO Tag",
+  //       href: "/equipment-mng/geo-tag",
+  //       icon: <SubdirectoryArrowRightIcon fontSize="small" />,
+  //     },
+  //     {
+  //       title: "Inspection Template",
+  //       href: "/equipment-mng/inspection-template",
+  //       icon: <SubdirectoryArrowRightIcon fontSize="small" />,
+  //     },
+  //   ],
+  // },
   {
     title: "Occupational Health",
     href: "/occupational-health",
