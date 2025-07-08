931b79ae31b9be2c8558809ebfb3626e78e1a305
AkilaManu <akilamanujith@gmail.com>
2025-07-08 09:07:36 +0530

    rag dashboard route visible

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -605,22 +605,14 @@ const AppRoutes = () => {
             ]
           )}
         />
-        {/* <Route
+        <Route
           path="/rag/dashboard"
           element={withLayout(
             MainLayout,
             RAGDashBoard,
             !userPermissionObject?.[PermissionKeys.RAG_DASHBOARD_VIEW]
           )}
-        /> */}
-
-        <Route
-          path="/rag/dashboard"
-          element={withLayout(MainLayout, () => (
-            <UnderDevelopment pageName="RAG System > RAG Management" />
-          ))}
         />
-
         <Route
           path="/rag/register"
           element={withLayout(
